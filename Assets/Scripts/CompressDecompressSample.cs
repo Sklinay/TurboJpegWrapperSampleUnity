8745bd35bf54bb3822b26057ed3aae0d2de864ee
CyrilC <kirua29@yahoo.fr>
2019-06-19 23:58:49 +0200

    Initial commit

---
+++ Assets/Scripts/CompressDecompressSample.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using TurboJpegWrapper;
+using UnityEngine;
+
+public class CompressDecompressSample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        TJDecompressor decompressor = new TJDecompressor();
+        TJCompressor compressor = new TJCompressor();
+
+        byte[] compressedImage = File.ReadAllBytes("Assets/Images/image.jpg");
+
+        float startTime = Time.realtimeSinceStartup;
+        DecompressedImage decompressedImage = decompressor.Decompress(compressedImage, TJPixelFormat.RGBA, TJFlags.None);
+        Debug.Log("Image decompressed. Execution time : " + (Time.realtimeSinceStartup - startTime).ToString() + " secondes.");     
+
+        
+        startTime = Time.realtimeSinceStartup;
+        byte[] recompressedImage = compressor.Compress(decompressedImage.Data, decompressedImage.Stride, decompressedImage.Width, decompressedImage.Height, decompressedImage.PixelFormat, TJSubsamplingOption.Chrominance420, 92, TJFlags.None);
+        Debug.Log("Image recompressed. Execution time : " + (Time.realtimeSinceStartup - startTime).ToString() + " secondes.");
+
+        File.WriteAllBytes("Assets/Images/recompressedImage.jpg", recompressedImage);
+        Debug.Log("Recompressed image saved at : Assets/Images/recompressedImage.jpg");
+        ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Example of using [https://github.com/Sklinay/AS.TurboJpegWrapperForUnity](AS.TurboJpegWrapperForUnity)
+
+This Unity project shows you how should be structured an Unity project to use [https://github.com/Sklinay/AS.TurboJpegWrapperForUnity](AS.TurboJpegWrapperForUnity).
+Tested on OSX and Windows 10.
+
+It uses [https://github.com/Sklinay/AS.TurboJpegWrapperForUnity](AS.TurboJpegWrapperForUnity) which is a fork of [https://github.com/quamotion/AS.TurboJpegWrapper](AS.TurboJpegWrapper)
+
